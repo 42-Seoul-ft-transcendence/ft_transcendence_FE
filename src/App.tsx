8613e91cc1c389de2fab99b0de5e857266897997
Your Name <you@your-domain.com>
2025-03-30 13:46:41 +0000

    ADD: MainPage and router table

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import './index.css'
 import './components/MainPage.tsx';
 import MainPage from './components/MainPage.tsx';
 
+import {Route, Routes, Navigate } from 'react-router-dom'
+
 function App() {
 
   return (
-    <>
-      <MainPage />
-    </>
+    <Routes>
+      <Route index element={<Navigate to={"/main"} replace />} />
+      <Route path="/main" element={<MainPage />} />
+      {/* <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/lobby" element={<ProtectedRoute><LobbyPage /></ProtectedRoute>} />
+      <Route path="/room/:roomid" element={<ProtectedRoute><RoomPage /></ProtectedRoute>} />
+      <Route path="/game/:gameid" element={<ProtectedRoute><GamePage /></ProtectedRoute>} /> */}
+    </Routes>
   )
 }
 
