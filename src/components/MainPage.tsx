56767783aabf5c89199b6619f4cd71981764017c
Chaerrrish <dhcofls0899@naver.com>
2025-03-30 19:33:14 +0900

    [setting] tailwind css setting

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BackGround from '../assets/background.png';
+
+function MainPage() {
+  return (
+    <div>
+        <BackGround />
+    </div>
+  )
+}
+
+export default MainPage
