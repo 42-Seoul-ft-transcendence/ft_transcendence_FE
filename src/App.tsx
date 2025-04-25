1475b6c7037f7935fc7038ab4bbc431be658dfaa
Your Name <you@your-domain.com>
2025-04-25 08:01:41 +0000

    add: login 2fa optins

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './index.css'
 import './components/MainPage.tsx';
 import MainPage from './components/MainPage.tsx';
 import LoginPage from './components/LoginPage.tsx';
+import AuthenticatePage from './components/AuthenticatePage.tsx'
 import RegisterPage from './components/RegisterPage.tsx';
 import VerifyPage from './components/VerifyPage.tsx';
 import LobbyPage from './components/LobbyPage.tsx';
 import RoomListPage from './components/RoomListPage.tsx';
-import RoomPage from './components/RoomPage.tsx';
 import GamePage from './components/GamePage.tsx';
 import ProtectedRoute from './components/ProtectedRoute.tsx';
 import NotFoundPage from './components/NotFoundPage.tsx';
@@ -21,11 +21,11 @@ function App() {
       <Route index element={<Navigate to={"/main"} replace />} />
       <Route path="/main" element={<MainPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/verify" element={<VerifyPage />} />
+      <Route path="/authenticate" element={<AuthenticatePage />} />
+      <Route path="/register" element={<ProtectedRoute><RegisterPage /></ProtectedRoute>} />
+      <Route path="/verify" element={<ProtectedRoute><VerifyPage /></ProtectedRoute>} />
       <Route path="/lobby" element={<ProtectedRoute><LobbyPage /></ProtectedRoute>} />
       <Route path="/rooms" element={<ProtectedRoute><RoomListPage /></ProtectedRoute>} />
-      <Route path="/room/:roomid" element={<ProtectedRoute><RoomPage /></ProtectedRoute>} />
       <Route path="/game/:gameid" element={<ProtectedRoute><GamePage /></ProtectedRoute>} />
       <Route path="/*" element={<NotFoundPage />} />
     </Routes>
